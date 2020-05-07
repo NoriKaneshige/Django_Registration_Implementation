d094e457535ef004f569df8a13ee31a325f9eef5
NoriKaneshige <atsunori@umich.edu>
2020-05-06 20:22:13 -0400

    change email

--- register/views.py
+++ register/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.views import (
     PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 )
 from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import send_mail
 
 # django.core.signing.dump generates token. This is based on SECRET_KEY.
 # The generated url based on SECRET_KEY for registration is sent to user.
@@ -18,7 +19,7 @@ from django.template.loader import render_to_string
 from django.views import generic
 from .forms import (
     LoginForm, UserCreateForm, UserUpdateForm, MyPasswordChangeForm,
-    MyPasswordResetForm, MySetPasswordForm
+    MyPasswordResetForm, MySetPasswordForm, EmailChangeForm
 )
 
 # get_user_model function receives UserModel (default User or custom User) that is currently used.
@@ -169,4 +170,62 @@ class PasswordResetConfirm(PasswordResetConfirmView):
 class PasswordResetComplete(PasswordResetCompleteView):
     """ password change compelete notification page """
     template_name = 'register/password_reset_complete.html'
+
+
+
+class EmailChange(LoginRequiredMixin, generic.FormView):
+    """メールアドレスの変更"""
+    template_name = 'register/email_change_form.html'
+    form_class = EmailChangeForm
+
+    def form_valid(self, form):
+        user = self.request.user
+        new_email = form.cleaned_data['email']
+
+        # URLの送付
+        current_site = get_current_site(self.request)
+        domain = current_site.domain
+        context = {
+            'protocol': 'https' if self.request.is_secure() else 'http',
+            'domain': domain,
+            'token': dumps(new_email),
+            'user': user,
+        }
+
+        subject = render_to_string('register/mail_template/email_change/subject.txt', context)
+        message = render_to_string('register/mail_template/email_change/message.txt', context)
+        send_mail(subject, message, None, [new_email])
+
+        return redirect('register:email_change_done')
+
+
+class EmailChangeDone(LoginRequiredMixin, generic.TemplateView):
+    """メールアドレスの変更メールを送ったよ"""
+    template_name = 'register/email_change_done.html'
+
+
+class EmailChangeComplete(LoginRequiredMixin, generic.TemplateView):
+    """リンクを踏んだ後に呼ばれるメアド変更ビュー"""
+    template_name = 'register/email_change_complete.html'
+    timeout_seconds = getattr(settings, 'ACTIVATION_TIMEOUT_SECONDS', 60*60*24)  # デフォルトでは1日以内
+
+    def get(self, request, **kwargs):
+        token = kwargs.get('token')
+        try:
+            new_email = loads(token, max_age=self.timeout_seconds)
+
+        # 期限切れ
+        except SignatureExpired:
+            return HttpResponseBadRequest()
+
+        # tokenが間違っている
+        except BadSignature:
+            return HttpResponseBadRequest()
+
+        # tokenは問題なし
+        else:
+            User.objects.filter(email=new_email, is_active=False).delete()
+            request.user.email = new_email
+            request.user.save()
+            return super().get(request, **kwargs)
 
